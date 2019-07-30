7975c52c1967ecf58eaa028303a3c82fe2754db2
ryanashneil <ryanashneil@gmail.com>
2019-07-31 00:00:11 +0800

    print everything; hopefully find something

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,10 +1,16 @@
 import 'reflect-metadata';
-// import { config } from 'dotenv';
+import { config } from 'dotenv';
 import { createConnection } from 'typeorm';
 
 import { User } from './models/user/entity';
 
-// config();
+config();
+
+console.log('############################# DB_HOST', process.env.DB_HOST);
+console.log('############################# DB_USERNAME', process.env.DB_USERNAME);
+console.log('############################# DB_PASSWORD', process.env.DB_PASSWORD);
+console.log('############################# DB', process.env.DB);
+console.log('############################# DB_PORT', process.env.DB_PORT);
 
 const connectDB = createConnection({
     type: 'mysql',
@@ -12,9 +18,10 @@ const connectDB = createConnection({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
+    port: parseInt(process.env.DB_PORT || '3306'),
     entities: [User],
     synchronize: true,
-    port: parseInt(process.env.DB_PORT || '3306'),
+    logging: true
 });
 
 export default connectDB;
