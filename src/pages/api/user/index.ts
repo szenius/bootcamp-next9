e08de69200930c35f948ee18935f1bcc4f622f53
ryanashneil <ryanashneil@gmail.com>
2019-07-30 17:30:47 +0800

    debug routes by mocking

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,17 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { listUser, addUser } from 'server/models/user';
+// import { listUser, addUser } from 'server/models/user';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const getAllUsers = async () => {
-        const allUsers = await listUser();
-        res.json(allUsers);
+        res.json([{name: 'hello'}]);
+        // const allUsers = await listUser();
+        // res.json(allUsers);
     }
 
     const postUser = async () => {
-        const { name } = req.body;
-        const newUser = await addUser(name);
-        res.json(newUser);
+        res.json({name: 'hello'});
+        // const { name } = req.body;
+        // const newUser = await addUser(name);
+        // res.json(newUser);
     }
 
     switch (req.method) {
