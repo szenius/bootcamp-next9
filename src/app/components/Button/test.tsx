022991df19c117aeb7b0e4a3c036df08fe306bb0
ryanashneil <ryanashneil@gmail.com>
2019-07-31 21:07:52 +0800

    try failing test

--- src/app/components/Button/test.tsx
+++ src/app/components/Button/test.tsx
@@ -9,4 +9,11 @@ describe('<Button />', () => {
         expect(component.type()).toBe('button');
         expect(component.prop('children')).toEqual('Add');
     })
+
+    it('should render a [loading] class when loading prop is assigned to [button]', () => {
+        const mockAction = jest.fn()
+        const component = shallow(<Button title='Add' onClick={mockAction} loading={true} />);
+        expect(component.type()).toBe('button');
+        expect(component.prop('className')).toContain('button-loading');
+    })
 });
